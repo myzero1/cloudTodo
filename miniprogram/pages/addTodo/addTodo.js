0f064e450e2f10eb12c702a6a7547b0d29908f82
myzero1 <myzero1@sina.com>
2019-09-15 07:02:41 +0800

    upload image

--- miniprogram/pages/addTodo/addTodo.js
+++ miniprogram/pages/addTodo/addTodo.js
@@ -2,15 +2,44 @@ const db = wx.cloud.database()
 const todos = db.collection('todos')
 
 Page({
+  data:{
+    image:null
+  },
+  selectImage:function(){
+    wx.chooseImage({
+      success: res => {
+        console.log(res.tempFilePaths[0])
+
+        wx.cloud.uploadFile({
+          cloudPath:`${Math.floor(Math.random()*100000)}.png`,
+          filePath: res.tempFilePaths[0]
+        }).then(res => {
+          console.log(res.fileID)
+
+          this.setData({
+            image:res.fileID
+          })
+        }).catch(err => {
+          console.error(err)
+        })
+      }
+    })
+  },
   onSubmit:function(event){
     todos.add({
       data:{
-        title: event.detail.value.title
+        title: event.detail.value.title,
+        image:this.data.image
       }
     }).then(res => {
       wx.showToast({
         title: 'success',
-        icon: 'success'
+        icon: 'success',
+        success:res2 => {
+          wx.redirectTo({
+            url: `../todoInfo/todoInfo?id=${res._id}`,
+          })
+        }
       })
     })
   }

5ffb96545165417e12108aeb22604e1a71db5c34
myzero1 <myzero1@sina.com>
2019-09-16 18:27:28 +0800

    test http

--- cloudfunctions/http/index.js
+++ cloudfunctions/http/index.js
@@ -7,9 +7,10 @@ cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let getResponse = await got('httpbin.org/get') //get请求 用httpbin.org这个网址做测试 
+  // let getResponse = await got('httpbin.org/get') //get请求 用httpbin.org这个网址做测试 
+  let getResponse = await got('https://open.onebox.so.com/api/getkuaidi?callback=jQuery18301815113148843135_1568628700707&com=huitongkuaidi&nu=71763507299371&time=1568628942&token=4f701a3b3933f735d496536def991846&_=1568628942321')
   return getResponse.body
-  
+
   let postResponse = await got('httpbin.org/post', {
     method: 'POST', //post请求
     headers: {
